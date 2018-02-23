6dcc6e514b086d557a1864a4d8e36bb23fb9a787
Friso Van der Veen <frisovanderveen@gmail.com>
2018-02-23 17:14:24 +0100

    bot: added RWMutex

--- bot.go
+++ bot.go
@@ -13,7 +13,7 @@ import (
 
 // Bot is a wrapper for a discordgo session
 type Bot struct {
-	mu sync.RWMutex
+	Mu sync.RWMutex
 
 	Session *discordgo.Session
 	Time    time.Time
@@ -59,11 +59,17 @@ func NewBot(options ...OptionFunc) (*Bot, error) {
 
 // Error writes the error to the ErrWriter
 func (b *Bot) Error(err error) {
+	b.Mu.Lock()
+	defer b.Mu.Unlock()
+
 	fmt.Fprintln(b.ErrWriter, b.ErrPrefix(), err)
 }
 
 // Open opens the discord session
 func (b *Bot) Open() error {
+	b.Mu.Lock()
+	defer b.Mu.Unlock()
+
 	if err := b.Session.Open(); err != nil {
 		return err
 	}
@@ -75,6 +81,9 @@ func (b *Bot) Open() error {
 
 // Close closes the discord session
 func (b *Bot) Close() error {
+	b.Mu.Lock()
+	defer b.Mu.Unlock()
+
 	return b.Session.Close()
 }
 
@@ -88,11 +97,16 @@ func (b *Bot) Restart() error {
 
 // AddHandler adds a handler to the discordgo session
 func (b *Bot) AddHandler(handler interface{}) {
+	b.Mu.Lock()
+	defer b.Mu.Unlock()
+
 	b.Session.AddHandler(handler)
 }
 
 // AddCommand adds a command to the bot
 func (b *Bot) AddCommand(coms ...*Command) error {
+	b.Mu.Lock()
+	defer b.Mu.Unlock()
 	double := false
 
 	for _, com := range coms {
@@ -112,10 +126,16 @@ func (b *Bot) AddCommand(coms ...*Command) error {
 
 // UpdateStatus sets the bot's status if game == "" then set status to active and not playing anything
 func (b *Bot) UpdateStatus(game string) error {
+	b.Mu.Lock()
+	defer b.Mu.Unlock()
+
 	return b.Session.UpdateStatus(0, game)
 }
 
 // UpTime is the time the bot has been active for
 func (b Bot) UpTime() time.Duration {
+	b.Mu.RLock()
+	defer b.Mu.RUnlock()
+
 	return time.Since(b.Time)
 }
